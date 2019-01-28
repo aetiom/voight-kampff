0419a796a9dd271a6ab150d0fdfa9877678c91db
Aetiom <aetiom@protonmail.com>
2019-01-28 10:28:17 +0100

    Display / VoightKampff Merge
    Merge Display and VoightKampff class. Now VoightKampff do display action alone

--- src/VoightKampff.php
+++ src/VoightKampff.php
@@ -1,21 +1,39 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of VoightKampff
+ * VoightKampff
  *
- * @author Alexandre
+ * @author Aetiom <aetiom@protonmail.com>
+ * @package VoightKampff
+ * @version 1.0
  */
 class VoightKampff {
     
-    const ERR_UNKNOWN_CAPTCHA_ID = 0;
+    /**
+     * @const integer ERR_UNKNOWN_CAPTCHA_ID : 
+     * Exception code for unknown captcha id
+     */
+    const ERR_UNKNOWN_CAPTCHA_ID = 101;
     
-    const ERR_CAPTCHA_ID_ALREADY_USED = 0;
+    /**
+     * @const integer ERR_CAPTCHA_ID_ALREADY_USED : 
+     * Exception code for captcha id that are already used by the system
+     */
+    const ERR_CAPTCHA_ID_ALREADY_USED = 102;
+    
+    /**
+     * @const integer ERR_FRONTEND_CLASS_MISSING :
+     * Esception code for missing frontend class in options/parameters
+     */
+    const ERR_FRONTEND_CLASS_MISSING = 201;
+    
+    /**
+     * @const integer ERR_FRONTEND_CLASS_UNKNOWN : 
+     * Exception code for unknown frontend class, if frontend class does not 
+     * exist for the current system
+     */
+    const ERR_FRONTEND_CLASS_UNKNOWN = 202;
+    
     
     
     /**
@@ -23,10 +41,22 @@ class VoightKampff {
      */
     protected $options;
     
-    
+    /**
+     * @var array $captcha : array of \VoightKampff\Captcha objects 
+     * with captcha collection 'id' as key
+     */
     protected $captcha = array();
     
+    /**
+     * @var \VoightKampff\Frontend $frontend : current frontend instance
+     */
+    protected $frontend;
+    
+    /**
+     * @var string $lang : current language
+     */
     protected $lang;
+    
     
     
     /**
@@ -64,8 +94,10 @@ class VoightKampff {
         $this->options['debug'] = $state;
     }
     
-    
-    
+    /**
+     * Set pool
+     * @param array $pool : pool items
+     */
     public function setPool(array $pool)
     {
         $this->options['pool'] = $pool;
@@ -191,6 +223,8 @@ class VoightKampff {
      */
     public function display($identifiers = null)
     {
+        $this->initiateFrontend();
+        
         if ($identifiers === null) {
             return $this->display(array_keys($this->captcha));
         }
@@ -267,20 +301,41 @@ class VoightKampff {
                     self::ERR_UNKNOWN_CAPTCHA_ID);
         }
         
-        $display = new \VoightKampff\Display($this->captcha[$id]->getImages(), array(
-            'frontend'  => $this->options['frontend'],
-            'cbPrefix'  => $this->options['cbPrefix'],
-            'cssEnable' => true,
-            'jsEnable'  => true,
-            'debug'     => $this->options['debug']
-        ));
+        $this->frontend->setCollection($this->captcha[$id]->getImages());
         
         $error = '';
         if ($this->captcha[$id]->getError() !== null) {
             $error = $this->captcha[$id]->getError()->getMessage($this->lang);
         }
         
-        return $display->getHtmlCode(
-                $this->captcha[$id]->getDirective($this->lang), $error);
+        return $this->frontend->createHtml(array (
+            'directive' => $this->captcha[$id]->getDirective($this->lang),
+            'error' => $error
+        ));
+    }
+    
+    /**
+     * Initiate Frontend instance
+     * @throws \Exception if frontend class is missing or unknown
+     */
+    private function initiateFrontend()
+    {
+        if (!isset($this->options['frontend']['class']) 
+                || empty($this->options['frontend']['class'])) {
+            throw new \Exception('frontend class is not set in options', 
+                    self::ERR_FRONTEND_CLASS_MISSING);
+        }
+        
+        $feClass = $this->options['frontend']['class'];
+        if (strstr('\\', $feClass) === false) {
+            $feClass = 'FrontEnd\\'.$feClass;
+        }
+        
+        if (!class_exists($feClass)) {
+            throw new \Exception('frontend class "'.$feClass.'" does not exist', 
+                    self::ERR_FRONTEND_CLASS_UNKNOWN);
+        }
+        
+        $this->frontend = new $feClass($this->options['frontend']);
     }
 }
